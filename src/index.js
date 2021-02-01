e6cb2f8beb657b7bb52b63eddf1866ca64f8cc98
EetuLeppanen <64865883+EetuLeppanen@users.noreply.github.com>
2021-02-02 01:19:20 +0200

    first snippet of code
    not functional search

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SearchBar from './SearchBar';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SearchBar />
   </React.StrictMode>,
   document.getElementById('root')
 );
