d2084cb7f6842820f052858c1aa783114340089a
Jkirvesmaki <joonas.kirvesmaki@gmail.com>
2021-03-11 13:34:56 +0200

    viilailua

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,11 +5,10 @@ import './App.css';
 
 function SearchBar () {
 
-  
-  
-
+  const [data, setData] = useState({"name":null});
   const [searchterm, setSearchterm] = React.useState('');
   
+
 
   const editSearchterm = (e) => setSearchterm(e.target.value);
 
@@ -30,23 +29,9 @@ function SearchBar () {
   });
 
 
-  
-
-  const [data, setData] = useState({"name":null});
-  const targetUrl = 'http://46.101.128.190:9200/testataan/_doc/_search?q=name:*'
-
-  useEffect(() => {
-    fetch(targetUrl)
-    .then(response => response.json())
-    .then(data => setData(data._source))
-    .catch(error => console.error(error))
-}, []);
-
 const dynamicSearch = () => {
  // return {data.name}.filter({data.name} => {data.name}.toLowerCase().includes(searchterm.toLowerCase())).sort()
 }
-
- 
 
   
 console.log(JSON.stringify(data))
@@ -58,7 +43,7 @@ console.log(JSON.stringify(data))
            <br></br>
           <h3>Hakuusi sopivat nimet:</h3>
           <div>
-          {data}  {dynamicSearch()}
+          {data.name}  
           
 
           </div>
